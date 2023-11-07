1904c44471e6ef6ed02d1d5eabf261e81a8e0f64
Keno Fischer <keno@juliacomputing.com>
2023-11-06 19:17:32 -0500

    Fixes

--- src/TESmartKVM.jl
+++ src/TESmartKVM.jl
@@ -10,6 +10,8 @@ ip and port.
 """
 module TESmartKVM
 
+public Device
+
 using Sockets
 
 # TODO: Could support RS232
@@ -25,7 +27,7 @@ struct TCPConnection <: Connection
 end
 
 function rpc!(c::TCPConnection, cmd::NTuple{6, UInt8})
-    write(getfield(c, :socket), cmd)
+    write(getfield(c, :socket), [cmd...])
     tuple(read(getfield(c, :socket), 6)...)
 end
 
@@ -71,6 +73,8 @@ function Base.getproperty(c::TCPConnection, s::Symbol)
         @assert reply[1:3] == "PT:"
         @assert reply[end] == ';'
         return parse(UInt16, reply[4:end])
+    else
+        return getfield(c, s)
     end
 end
 
@@ -79,32 +83,37 @@ function Base.setproperty!(c::TCPConnection, s::Symbol, v)
         ui = UInt8(v::Integer)
         reply = rpc!(c, (0xAA, 0xBB, 0x03, 0x01, ui, 0xEE))
         @assert reply[1:4] == (0xaa, 0xbb, 0x03, 0x11)
-        @assert reply[6] == 0x16
-        # The read is zero-indexed in the protocol. For consistency, both with
-        # julia and the labeling on the device, we use 1 indexing everywhere
-        @assert reply[5] == ui
+        @show reply
+        @assert reply[5] == ui - 1
+        @assert reply[6] == 0x15+ui
         return ui
     elseif s === :muted
-        write(c.socket, (0xAA, 0xBB, 0x03, 0x02, v ? 0x01 : 0x00, 0xEE))
+        write(getfield(c, :socket), [0xAA, 0xBB, 0x03, 0x02, v ? 0x00 : 0x01, 0xEE])
     elseif s === :active_input_detection
-        write(c.socket, (0xAA, 0xBB, 0x03, 0x81, v ? 0x01 : 0x00, 0xEE))
+        write(getfield(c, :socket), [0xAA, 0xBB, 0x03, 0x81, v ? 0x01 : 0x00, 0xEE])
     elseif s === :ip
         v::IPv4Addr
+        @warn("IP address changes to not take effect until the next reboot")
         rpc!(c, "IP: $v;")
     elseif s === :gateway
         v::IPv4Addr
+        @warn("Gateway address changes to not take effect until the next reboot")
         rpc!(c, "GW: $v;")
     elseif s === :netmask
         # TOOD: Julia should probably have a dedicated netmask type
         v::IPv4Addr
+        @warn("Netmask changes to not take effect until the next reboot")
         rpc!(c, "MA: $v;")
     elseif s === :port
+        @warn("Port changes to not take effect until the next reboot")
         rpc!(c, "PT: $(string(v::UInt16, base=10));")
+    else
+        return setfield!(c, s, v)
     end
 end
 
 function Sockets.connect(d::Device)
-    TCPConnection(Sockets.connect(d.ip, d.port))
+    TCPConnection(d, Sockets.connect(d.ip, d.port))
 end
 
 end
